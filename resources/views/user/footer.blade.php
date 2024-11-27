e0fa755b607f77677e48525e24b185a27bff9e2c
erdevkumar7 <erdevkumar7@gmail.com>
2024-11-27 18:55:45 +0530

    add social link in footer

--- resources/views/user/footer.blade.php
+++ resources/views/user/footer.blade.php
@@ -1,13 +1,26 @@
 <footer id="footer" class="footer dark-background">
     <div class="container copyright text-center mt-4">
-        <p>© <span>Copyright</span> <strong class="px-1 sitename">MAAN(NAVOTSAV-3.0)</strong> <span>All Rights Reserved</span></p>
-        <div class="credits">
-            Developed by <a href="https://bootstrapmade.com/">Votive Technologies</a>
+        <div class="text-center">
+            <div class="social-links d-flex justify-content-center mb-4">
+                <a href="" class="me-3"><i class="bi bi-twitter"></i></a>
+                <a href="" class="me-3"><i class="bi bi-facebook"></i></a>
+                <a href="" class="me-3"><i class="bi bi-instagram"></i></a>
+                <a href=""><i class="bi bi-linkedin"></i></a>
+            </div>
         </div>
+        
+        <p>© <span>Copyright</span> <strong class="px-1 sitename">MAAN(NAVOTSAV-3.0)</strong> <span>All Rights
+                Reserved</span></p>
+        <div class="credits">
+            <div>
+            Developed by <a href="" target="_blank">Votive Technologies</a>
+        </div>     
+
     </div>
 </footer>
- <!-- Scroll Top -->
- <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+<!-- Scroll Top -->
+<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
+        class="bi bi-arrow-up-short"></i></a>
 
- <!-- Preloader -->
- <div id="preloader"></div>
+<!-- Preloader -->
+<div id="preloader"></div>
